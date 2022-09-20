44a45ca5552d9f42b3e935003f63eff48786ce3c
Sexy-Zombie <szilgyigbor@gmail.com>
2022-09-20 19:39:12 +0200

    Just added more product instances. testing.

--- src/Codecool.CodecoolShop/Startup.cs
+++ src/Codecool.CodecoolShop/Startup.cs
@@ -71,9 +71,27 @@ namespace Codecool.CodecoolShop
             supplierDataStore.Add(lenovo);
             ProductCategory tablet = new ProductCategory {Name = "Tablet", Department = "Hardware", Description = "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display." };
             productCategoryDataStore.Add(tablet);
+            ProductCategory toy = new ProductCategory {Name = "Toy", Department = "Games", Description = "A little game for everyone." };
+            productCategoryDataStore.Add(toy);
             productDataStore.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "USD", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
             productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
             productDataStore.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = tablet, Supplier = amazon });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+            productDataStore.Add(new Product { Name = "Amazon Fire", DefaultPrice = 49.9m, Currency = "USD", Description = "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", ProductCategory = tablet, Supplier = amazon });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+            productDataStore.Add(new Product { Name = "Amazon Fire HD 8", DefaultPrice = 89.0m, Currency = "USD", Description = "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", ProductCategory = tablet, Supplier = amazon });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+            productDataStore.Add(new Product { Name = "Lenovo IdeaPad Miix 700", DefaultPrice = 479.0m, Currency = "USD", Description = "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", ProductCategory = tablet, Supplier = lenovo });
+
+            productDataStore.Add(new Product { Name = "Vibrátor szopó automata", DefaultPrice = 33.0m, Currency = "USD", Description = "The best machine.", ProductCategory = toy, Supplier = amazon });
+            productDataStore.Add(new Product { Name = "Vibrátor szopó automata", DefaultPrice = 33.0m, Currency = "USD", Description = "The best machine.", ProductCategory = toy, Supplier = amazon });
+            productDataStore.Add(new Product { Name = "Vibrátor szopó automata", DefaultPrice = 33.0m, Currency = "USD", Description = "The best machine.", ProductCategory = toy, Supplier = amazon });
+            productDataStore.Add(new Product { Name = "Vibrátor szopó automata", DefaultPrice = 33.0m, Currency = "USD", Description = "The best machine.", ProductCategory = toy, Supplier = amazon });
+            productDataStore.Add(new Product { Name = "Vibrátor szopó automata", DefaultPrice = 33.0m, Currency = "USD", Description = "The best machine.", ProductCategory = toy, Supplier = amazon });
+
         }
     }
 }
