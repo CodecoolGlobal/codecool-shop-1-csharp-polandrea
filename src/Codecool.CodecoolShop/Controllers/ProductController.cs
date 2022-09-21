8c383550a7d91186df52952e70e007a25e3dbd16
Sexy-Zombie <szilgyigbor@gmail.com>
2022-09-21 11:38:34 +0200

    Cart method to controller, and cart.cshtml are added.

--- src/Codecool.CodecoolShop/Controllers/ProductController.cs
+++ src/Codecool.CodecoolShop/Controllers/ProductController.cs
@@ -46,5 +46,12 @@ namespace Codecool.CodecoolShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+
     }
 }
