483decda0620a31fb033ce768cb4330132a996e0
Sexy-Zombie <szilgyigbor@gmail.com>
2022-09-21 22:25:15 +0200

    Now you can add products to cart from the filtered page.

---
+++ src/Codecool.CodecoolShop/wwwroot/js/Filtered.js
@@ -0,0 +1,21 @@
+﻿var addButtons = document.querySelectorAll("#cart-button");
+
+for (let i of addButtons) {
+    i.addEventListener('click', async () => {
+        console.log(i)
+        apiPost("/api/AddToCart", i.dataset.id)
+
+    })
+}
+
+
+async function apiPost(url, payload) {
+    let data = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+    return await data.json()
+}
